1f9cafca1c8fa271abc4af49bbe70aa46e158b6a
Kaz Nishimura <kazssym@vx68k.org>
2014-11-04 16:44:29 +0900

    Added a ToolInstallation-derived class.

---
+++ src/main/java/org/vx68k/jenkins/plugin/bds/BDSInstallation.java
@@ -0,0 +1,85 @@
+/*
+ * BDSInstallation
+ * Copyright (C) 2014 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vx68k.jenkins.plugin.bds;
+
+import java.io.IOException;
+import java.util.List;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.model.EnvironmentSpecific;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.slaves.NodeSpecific;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolProperty;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Lets users select RAD Studio installations in their projects.
+ *
+ * @author Kaz Nishimura
+ * @since 1.0
+ */
+public class BDSInstallation extends ToolInstallation implements
+        NodeSpecific<BDSInstallation>, EnvironmentSpecific<BDSInstallation> {
+
+    private static final String DISPLAY_NAME = "RAD Studio";
+
+    @DataBoundConstructor
+    public BDSInstallation(String name, String home,
+                List<? extends ToolProperty<?>> properties) {
+        super(name, home, properties);
+    }
+
+    @Override
+    public BDSInstallation forNode(Node node, TaskListener log) throws
+            IOException, InterruptedException {
+        return new BDSInstallation(getName(), translateFor(node, log),
+                getProperties().toList());
+    }
+
+    @Override
+    public BDSInstallation forEnvironment(EnvVars environment) {
+        return new BDSInstallation(getName(), environment.expand(getHome()),
+                getProperties().toList()); // TODO: Expand other variables.
+    }
+
+    /**
+     * Describes {@link BDSInstallation}.
+     *
+     * @author Kaz Nishimura
+     */
+    @Extension
+    public static class Descriptor extends ToolDescriptor<BDSInstallation> {
+
+        public Descriptor() {
+        }
+
+        /**
+         * Returns the display name of this plugin.
+         *
+         * @return the display name
+         */
+        @Override
+        public String getDisplayName() {
+            return DISPLAY_NAME;
+        }
+    }
+}
